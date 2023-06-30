e143f1ae4b000c4adfa74f5442c8d083732bf7fd
BenjaminPoh <e0726717@u.nus.edu>
2023-06-30 14:38:50 +0800

    i love priority
    also where .gitignore

--- src/main/java/benloti/holoquiz2/handlers/TestHandler.java
+++ src/main/java/benloti/holoquiz2/handlers/TestHandler.java
@@ -5,12 +5,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class TestHandler implements Listener {
     public TestHandler(HoloQuiz2 plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    //only in this shibal import that a lower priority run before the higher ones
+    public void onTorchPlaced2(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+        if(block.getType() == Material.TORCH) {
+            block.setType(Material.WARPED_HYPHAE);
+        }
+        Bukkit.getLogger().info("yeetus");
     }
 
     @EventHandler
